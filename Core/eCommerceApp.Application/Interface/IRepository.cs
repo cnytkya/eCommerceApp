468b11b11235578938cc478b07e536a43f27e129
kayac <kayac@CUNEYT>
2025-07-12 11:54:24 +0300

    Add user management features and restructure projects
    - Updated `eCommerceApp.Application.csproj` to reference `eCommerceApp.Domain`.
    - Modified `eCommerceApp.Infrastructure.csproj` to reference `eCommerceApp.Application`.
    - Enhanced admin navigation by adding a link to the `Users` controller in `_AdminLayout.cshtml`.
    - Created `IRepository.cs` for a generic repository interface.
    - Introduced `Repository.cs` implementing the `IRepository` interface.
    - Added `UsersController.cs` with an `Index` action method for user management.
    - Created `Index.cshtml` to display a detailed user list in a table format.

---
+++ Core/eCommerceApp.Application/Interface/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.Application.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(string id);//IdentityUser'lar string ID kullanır
+        Task<T?> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate);//Belirtilen koşula uyan ilk varlığı asenkron olarak getir.
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
---
+++ Infrastructure/eCommerceApp.Infrastructure/Persistence/Repositories/Repository.cs
@@ -0,0 +1,34 @@
+﻿using eCommerceApp.Application.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace eCommerceApp.Infrastructure.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext _appDbContext;
+        //Doğrudan erişim dbset kullanılabilir.
+        protected readonly DbSet<T> _dbSet;
+
+        public Repository(DbSet<T> dbSet, AppDbContext appDbContext)
+        {
+            _dbSet = dbSet;
+            _appDbContext = appDbContext;
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
