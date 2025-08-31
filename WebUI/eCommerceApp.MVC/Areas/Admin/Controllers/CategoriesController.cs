b4d3725dd1d0aa21e6589bbaf072282c3ebc0988
kayac <kayac@CUNEYT>
2025-08-31 12:45:26 +0300

    Add category management features and navigation links
    - Updated `_AdminLayout.cshtml` to include navigation links for "Products" and "Categories" using ASP.NET Core routing.
    - Introduced `CategoriesController` in the Admin area for handling category operations with dependency injection for `ICategoryService`.
    - Added `Index.cshtml` to display a list of categories with a structured UI, including action buttons for editing and deleting categories.

---
+++ WebUI/eCommerceApp.MVC/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using eCommerceApp.Application.Interface.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceApp.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        //DI ile kategori CRUD için ICategoryService i buraya inject(uygula) ediyoruz.
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        //kategori listeleme sayfasının metodu aynı zamanda kategorinin ana sayfası.
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllCategoriesAsync();
+            return View(categories);
+        }
+    }
+}
